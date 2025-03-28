1a716056a4e18bed1b5ad9555245729fa72c1671
shiqi <liziw2012@gmail.com>
2025-03-28 16:10:45 +0800

    initial

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/components/button";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full text-center">
+      <h1 className="text-3xl font-bold">Hello Next</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
